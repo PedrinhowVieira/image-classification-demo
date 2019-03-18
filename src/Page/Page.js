73c7a088693b70ad0b862656d04ca46a3faae5f6
PedrinhowVieira <pedro.dmvieira@gmail.com>
2019-03-17 21:07:00 -0300

    add style customization

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -1,34 +1,39 @@
 import React, { Component } from "react";
 import { Nav, Navbar, NavDropdown, Button, Form, FormControl, ListGroup, Col , Row, Container, Image, InputGroup} from 'react-bootstrap';
+import './Page.css'
 
 class Page extends Component {
   render() {
     return(
       <div name='page'>
-
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
-
         <Container fluid='true'>
+
+          <Row>
+            <Navbar bg="light" expand="lg">
+              <Navbar.Brand href="#home">
+                <Image src={require('./logo.png')} width="40" height="40"/>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                </Nav>
+                <Form inline>
+                  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+                <NavDropdown title="User" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Next Image</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Refresh</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Log Off</NavDropdown.Item>
+                </NavDropdown>
+              </Navbar.Collapse>
+            </Navbar>
+          </Row>
+
           <Row>
             <InputGroup className="mb-3">
               <InputGroup.Prepend>
@@ -39,22 +44,34 @@ class Page extends Component {
               <FormControl id="basic-url" aria-describedby="basic-addon3" />
             </InputGroup>
           </Row>
+
           <Row>
-              <Col md={2}>
-                <ListGroup defaultActiveKey="#link1">
-                  <ListGroup.Item action href="#link1">
-                    Link 1
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link2" disabled>
-                    Link 2
-                  </ListGroup.Item>
-                  <ListGroup.Item action>This one is a button</ListGroup.Item>
-                </ListGroup>
-              </Col>           
-              <Col md={10}>
-                <Image src={require('./gallery.jpg')} fluid='true' />;
-              </Col>
+            <Col md={2}>
+              <ListGroup defaultActiveKey="#tags">
+                <ListGroup.Item action href="#enhance">
+                  Enhance
+                </ListGroup.Item>
+                <ListGroup.Item action href="#document-type">
+                  Document Type
+                </ListGroup.Item>
+                <ListGroup.Item action href="#document-title">
+                  Document Title
+                </ListGroup.Item>
+                <ListGroup.Item action href="#receipt">
+                  Receipt
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>           
+            <Col md={10}>
+              <Row>
+                <Image src={require('./gallery.jpg')} fluid='true' className='gallery'/>
+              </Row>
+              <Row>
+                <Button variant="primary" className='buttonzin'>Primary</Button>
+              </Row>
+            </Col>
           </Row>
+
         </Container>
       </div>
     );
