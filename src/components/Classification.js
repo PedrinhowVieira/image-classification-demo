289f581a76cdb1aad856694ddbe61b1ea8b5e84b
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2019-03-20 17:25:19 +0000

    Refactor Classification into functional component

--- src/components/Classification.js
+++ src/components/Classification.js
@@ -1,11 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Classification extends Component {
-  render() {
-    return (
-      <img src={require('./../Images/gallery2.jpeg')} alt=""/>
-    );
-  }
-}
+const classification = () => (
+  <img src={require('./../Images/gallery2.jpeg')} alt=""/>
+);
 
-export default Classification;
+export default classification;
