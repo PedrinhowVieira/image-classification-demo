07107f765f5cc67e3f47079d5df176882e2e0fb8
PedrinhowVieira <pedro.dmvieira@gmail.com>
2019-03-21 10:32:52 -0300

    index and app updates

--- src/App/App.js
+++ src/App/App.js
@@ -1,40 +1,42 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, MemoryRouter, Route } from "react-router-dom";
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, MemoryRouter, Route, Redirect } from "react-router-dom";
 import { NoSsr, Grid } from '@material-ui/core/';
 import { UpperBar, SearchBar, SideList } from './../imports'
 import { Enhancement, Classification, Filename, Summary } from './../imports'
+import { withGlobalState } from 'react-globally'
 
-function App() {
-  return (
-    <Fragment>
-      <NoSsr>
-        <MemoryRouter initialEntries={['/enhancement']} initialIndex={0}>
-          <Router>
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <NoSsr>
+          <MemoryRouter initialEntries={['/enhancement']} initialIndex={0}>
+            <Router>
 
-            <UpperBar />
+              <UpperBar />
 
-            <SearchBar />
+              <SearchBar />
 
-            <Grid container spacing={12}>
-              <Grid item sm={2}>
+              <Grid container item xs={12} spacing={8}>
+                <Grid item xs={2}>
 
-                <SideList />
+                  <SideList />
 
+                </Grid>
+                <Grid item xs={10}>
+                  <Route exact path="/" render={() => (<Redirect to="/enhancement"/>)}/>
+                  <Route path="(/enhancement)" component={Enhancement} />
+                  <Route path="/classification" component={Classification} />
+                  <Route path="/filename" component={Filename} />
+                  <Route path="/summary" component={Summary} />
+                </Grid>
               </Grid>
-              <Grid item sm={10}>
-                <Route path="(/|/enhancement)" component={Enhancement} />
-                <Route path="/classification" component={Classification} />
-                <Route path="/filename" component={Filename} />
-                <Route path="/summary" component={Summary} />
-              </Grid>
-            </Grid>
-          </Router>
-        </MemoryRouter>
-      </NoSsr>
-    </Fragment>
-  );
+            </Router>
+          </MemoryRouter>
+        </NoSsr>
+      </Fragment>
+    );
+  }
 }
 
-
-
-export default App;
+export default withGlobalState(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import App from './App/App';
+import React from "react"
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-globally'
+import App from './App/App'
+
+const initialState = {
+  go_to:"",
+  enhancement: undefined,
+  classification: undefined,
+  filename: undefined,
+}
 
 ReactDOM.render(
-  <App />,
+  <Provider globalState={initialState}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
 
